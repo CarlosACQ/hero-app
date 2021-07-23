0c3d4ee891b5a349fb6d9f86a977f97e204b1711
Carlos Alberto Cabrera Quiroga <carloscabreraq@gmail.com>
2021-07-23 15:47:56 -0400

    primer commit

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,31 @@
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+import { Navbar } from '../components/ui/NavBar';
+import { HeroScreen } from '../components/hero/HeroScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { DcScreen } from '../components/dc/DcScreen';
+
+
+export const DashboardRoutes = () => {
+    return (
+        <Fragment>
+            <Navbar/>
+            <div>
+                <Switch>
+                    <Route  exact path="/marvel"  component ={MarvelScreen} />
+                    <Route  exact path="/heroe/:heroeId"  component ={HeroScreen} />
+                    <Route  exact path="/dc"  component ={DcScreen} />
+
+                    <Redirect to="/marvel"/>
+                </Switch>
+            </div>
+        </Fragment>
+    )
+}
